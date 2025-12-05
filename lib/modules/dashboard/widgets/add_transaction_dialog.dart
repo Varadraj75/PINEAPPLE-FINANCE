631b223ac9b037f24e87ef40b01315c011b58c38
Varadraj75 <agrawalvaradraj2007@gmail.com>
2025-12-05 18:04:40 +0530

    REMOVE APP RESET BUTTON

--- lib/modules/dashboard/widgets/add_transaction_dialog.dart
+++ lib/modules/dashboard/widgets/add_transaction_dialog.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:pineapple_finance/core/theme/app_colors.dart';
+// import 'package:pineapple_finance/core/theme/app_colors.dart';
 import 'package:pineapple_finance/data/database/database_helper.dart';
 import 'package:pineapple_finance/data/models/transaction_model.dart';
 
